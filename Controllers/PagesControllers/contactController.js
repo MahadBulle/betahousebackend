3bd6ed7b3ea4b412f34becaad3b4c8bf654d9970
MahadBulle <mahadhussein012@gmail.com>
2023-07-27 04:18:42 -0700

    first commit

---
+++ Controllers/PagesControllers/contactController.js
@@ -0,0 +1,60 @@
+const { ContactModel, contactValidation } = require('../../Models/PagesModels/ContactModel')
+// get start
+const GetContact = async (req, res) => {
+  // res.send("hello iam contact")
+  try {
+    const hellGetContact = await ContactModel.find()
+    res.status(200).send(hellGetContact)
+  } catch (error) {
+    res.send({ errors: error })
+  }
+}
+// get ended
+// get ById start
+const GetContactById = async (req, res) => {
+  try {
+    const hellGetContactById = await ContactModel.findById(req.params.id)
+    res.status(200).send(hellGetContactById)
+  } catch (error) {
+    res.send({ errors: error })
+  }
+}
+// get ById ended
+// post start
+const PostContact = async (req, res, next) => {
+  try {
+    const { error } = contactValidation(req.body)
+    if (error) return res.status(400).send(error.message)
+    const contactPosting = new ContactModel(req.body)
+    await contactPosting.save()
+    res.status(201).send({
+      status: true,
+      contactPosting,
+      message: 'succefully inserted '
+    })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+}
+// post ended
+// put/update houseImages start
+const PutContact = async (req, res) => {
+  try {
+    const ContactUpdating = await ContactModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).send({
+      status: 'Success',
+      message: 'Successfully Updated',
+      info: ContactUpdating
+    })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+}
+// put/update ended
+module.exports = {
+  GetContact,
+  GetContactById,
+  PostContact,
+  PutContact
+}
---
+++ routes/PagesRoutes/contactRout.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+// const { ContactModel } = require('../../Models/PagesModels/contactModel')
+
+// const contactController = require('../../Controllers/PagesControllers/contactController')
+// const contactController = require('../../Controllers/PagesControllers/contactController')
+const {GetContact,GetContactById,PostContact,PutContact} = require('../../Controllers/PagesControllers/contactController')
+// router.get('/',(req,res) => {
+
+//     res.send("waa uu shaqeenaa hada")
+// })
+router.get('/',GetContact)
+
+router.get('/:id',GetContactById)
+
+router.post('/', PostContact) 
+
+router.put('/:id',PutContact)
+
+module.exports = router
