3add03ba76344d84783ffbe58ac4c2f5f5a03406
MahadBulle <mahadhussein012@gmail.com>
2023-07-28 07:20:01 -0700

    cors change

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const servicesRoutes = require('./routes/PagesRoutes/ourServicesRoute')
 const app = express()
 
 app.use(express.json())
-app.use(cors({origin:['http://localhost:5173/']}));
+app.use(cors({origin:'http://localhost:5173/'}));
 
 // const Port = process.env.PORT
 //   app.listen(Port);
