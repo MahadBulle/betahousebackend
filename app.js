d31d4d441ac34ad1bfac8d037c8a0ba65d36ad9e
MahadBulle <mahadhussein012@gmail.com>
2023-08-01 21:25:23 -0700

    sug tir

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const servicesRoutes = require('./routes/PagesRoutes/ourServicesRoute')
 const app = express()
 
 app.use(express.json())
-app.use(cors({origin:'http://localhost:5173',origin:'https://beta-houses.vercel.app'}));
+app.use(cors({origin:'http://localhost:5173'}));
 
 // const Port = process.env.PORT
 //   app.listen(Port);
