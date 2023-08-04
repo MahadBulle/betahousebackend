0992c00fa0e3de557ab29803b753abf5a92fa1dc
MahadBulle <mahadhussein012@gmail.com>
2023-08-04 01:56:10 -0700

    about updated

--- Controllers/PagesControllers/aboutController.js
+++ Controllers/PagesControllers/aboutController.js
@@ -1,8 +1,19 @@
 const { AboutModel, aboutValidation } = require('../../Models/PagesModels/aboutModel')
+// // get start
+// const GetAbout = async (req, res) => {
+//   const hellGetAbout = await AboutModel.find()
+//   res.status(200).send(hellGetAbout)
+// }
+// // get ended
 // get start
 const GetAbout = async (req, res) => {
-  const hellGetAbout = await AboutModel.find()
-  res.status(200).send(hellGetAbout)
+  try {
+    const data = await AboutModel.find().sort({ _id: -1 }).limit(1)
+    // const data = await aboutModel.find().sort({createdAt:-1}).limit(1)
+    res.status(200).send(data)
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 }
 // get ended
 // get ById start
@@ -16,16 +27,41 @@ const PostAbout = async (req, res, next) => {
   try {
     const { error } = aboutValidation(req.body)
     if (error) return res.status(400).send(error.message)
-    const aboutPosting = new AboutModel(req.body)
-    await aboutPosting.save()
-    res.status(201).send({
-      status: true,
-      aboutPosting,
-      message: 'succefully inserted '
-    })
-  } catch (error) {
-    res.status(400).send(error.message)
+
+    const getabout = await AboutModel.find().sort({ _id: -1 }).limit(1)
+    if (!getData) {
+      const postabout = await AboutModel(req.body);
+      await postabout.save();
+      res.status(201).send({ status: true, message: 'successfully saved' });
+
+
+    } else {
+
+      await AboutModel.findByIdAndUpdate(getData[0]._id, req.body, { new: true });
+      res.status(201).send({ status: true, message: 'successfully updated' });
+
+    }
+
+
+  } 
+  catch (error) {
+    res.status(404).send(error.message);
   }
+
+
+
+
+
+  //   const aboutPosting = new AboutModel(req.body)
+  //   await aboutPosting.save()
+  //   res.status(201).send({
+  //     status: true,
+  //     aboutPosting,
+  //     message: 'succefully inserted '
+  //   })
+  // } catch (error) {
+  //   res.status(400).send(error.message)
+  // }
 }
 // post ended
 // put/update houseImages start
